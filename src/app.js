bdfada23e2c8b274c3dab9f183ca784b6589da5b
Gubar Vlad <endlessloop@bk.ru>
2025-05-18 19:38:38 +0400

    add module ClicksModule

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
 import './styles.css'
 import { ContextMenu } from './menu.js'
+import { ClicksModule } from './modules/clicks.module.js'
 
 const menu = new ContextMenu('#menu')
+
+menu.add(new ClicksModule('clicks-module', 'Аналитика кликов'))
