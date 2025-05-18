5854de74d70f4aa73a4d43dbfc380fdf19e78597
FrontEvEndJenia <evgenius007007@gmail.com>
2025-05-18 21:28:45 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ import { DogsPhotos } from './modules/randomDogPhoto.js'
 import { RandomFigure } from './modules/randomFigure.module.js'
 import { ClicksModule } from './modules/clicks.module.js'
 import { TimerModale } from './modules/timer-module.js'
+import { Complimenter } from './modules/compliment.module'
 
 const menu = new ContextMenu('#menu')
 
@@ -17,4 +18,5 @@ menu.add(new DogsPhotos())
 menu.add(new RandomFigure())
 menu.add(new ClicksModule())
 menu.add(new TimerModale())
+menu.add(new Complimenter())
 
